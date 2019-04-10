9240e11a1c3072f3e5dfd661ed7eea8c2b3ef9d6
zritah <reetagive@gmail.com>
2019-04-10 09:10:37 +0300

    Added notification to Report activity

--- app/src/main/java/com/example/classappbit/REPORT.java
+++ app/src/main/java/com/example/classappbit/REPORT.java
@@ -1,15 +1,34 @@
 package com.example.classappbit;
 
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.ann.classappbit2.R;
 
 public class REPORT extends AppCompatActivity {
+    private final String CHANNEL_ID = "personal_notifications";
+    private final int  NOTIFICATION_ID = 001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+    }
+
+    public void sendReport(View view) {
+
+        NotificationCompat.Builder builder = new  NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_sms_notification);
+        builder.setContentTitle("Sent Problem");
+        builder.setContentText("Problem has been sent");
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+
+
     }
 }
